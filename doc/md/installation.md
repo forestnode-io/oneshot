45034b375687ca1a3b5bf8b9596e2909a3939779
Raphael Reyna <raphaelreyna@protonmail.com>
2020-06-12 18:30:09 -0700

    Add customization to auto-generated README

---
+++ doc/md/installation.md
@@ -0,0 +1,21 @@
+### Installation
+
+There are multiple ways of obtaining oneshot:
+
+##### Brew
+```bash
+brew tap raphaelreyna/homebrew-repo
+brew install oneshot
+```
+
+##### Go get
+```bash
+go get -u -v github.com/raphaelreyna/oneshot
+```
+
+##### Compiling from source
+```bash
+git clone github.com/raphaelreyna/oneshot
+cd oneshot
+sudo make install
+```
--- doc/md/main.go
+++ doc/md/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"github.com/raphaelreyna/oneshot/cmd"
 	"github.com/spf13/cobra/doc"
-	"log"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	cmd.SetFlags()
-	err := doc.GenMarkdown(cmd.RootCmd, os.Stdout)
+	mdBuffer := &bytes.Buffer{}
+	err := doc.GenMarkdown(cmd.RootCmd, mdBuffer)
 	if err != nil {
-		log.Print(err)
+		panic(err)
 	}
+
+	parts := bytes.Split(mdBuffer.Bytes(), []byte(`### Synopsis`))
+	os.Stdout.Write(parts[0])
+
+	here, err := os.Open(".")
+	defer here.Close()
+	if err != nil {
+		panic(err)
+	}
+	files, _ := here.Readdirnames(0)
+	var file *os.File
+	for _, fName := range files {
+		if filepath.Ext(fName) == ".md" {
+			file, err = os.Open(fName)
+			if err != nil {
+				panic(err)
+			}
+			os.Stdout.Write([]byte("\n"))
+			io.Copy(os.Stdout, file)
+			file.Close()
+			os.Stdout.Write([]byte("\n"))
+		}
+	}
+
+	os.Stdout.Write([]byte("\n### Synopsis\n"))
+	os.Stdout.Write(parts[1])
 }
