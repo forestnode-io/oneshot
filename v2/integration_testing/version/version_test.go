68a1e70ab8c86d75e774f20e8b6dd395bc97cb66
Raphael Reyna <raphaelreyna@protonmail.com>
2023-01-26 12:13:04 -0800

    add version test

---
+++ v2/integration_testing/version/version_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	itest "github.com/raphaelreyna/oneshot/v2/integration_testing"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestBasicTestSuite(t *testing.T) {
+	suite.Run(t, new(ts))
+}
+
+type ts struct {
+	itest.TestSuite
+}
+
+func (suite *ts) Test_NoHang() {
+	var oneshot = suite.NewOneshot()
+	oneshot.Args = []string{"version"}
+	oneshot.Start()
+	defer oneshot.Cleanup()
+
+	oneshot.Wait()
+}
